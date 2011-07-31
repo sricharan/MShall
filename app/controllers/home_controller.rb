11797c6dfdaf30217d1ebff4b612b32a38a53924
sricharan sunder <sricharan@joshsoftware.com>
2011-07-31 20:34:10 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+
+ def index
+ end
+ 
+ def from_the_hall
+ end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MShall::Application.config.secret_token = 'c61a6ea6ca36802078a9d1c9dc48c977592878c1265737a5a3535656f2ee2bdff2c1bcf03d904bd1934f8c7d8ac05a9dec5fd290a87f840690321de887b61cc7'
